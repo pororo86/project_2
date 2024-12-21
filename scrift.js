a0a098d9d229322145fa28a5f897b1504c2a046e
pororo86 <s.adiwijayaa1@gmail.com>
2024-12-21 13:08:09 +0700

    Update scrift.js

--- scrift.js
+++ scrift.js
@@ -6,3 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+document.getElementById('search').addEventListener('input', function () {
+  const query = this.value.toLowerCase();
+  const products = document.querySelectorAll('.product-item');
+
+  products.forEach((product) => {
+    const productName = product.querySelector('summary').textContent.toLowerCase();
+    const features = product.querySelector('.product-features').textContent.toLowerCase();
+
+    if (productName.includes(query) || features.includes(query)) {
+      product.style.display = 'block'; // Tampilkan jika cocok
+    } else {
+      product.style.display = 'none'; // Sembunyikan jika tidak cocok
+    }
+  });
+});
